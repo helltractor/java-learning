a6213a5e28daa3caa700379217e94a7b12471702
helltractor <1664309610@qq.com>
2025-03-24 11:57:21 +0800

    feat(demo-redis-lettuce): add demo-redis-lettuce module, ref warpexchange

---
+++ demo-redis-lettuce/src/main/java/com/helltractor/demo/redis/RedisCache.java
@@ -0,0 +1,20 @@
+package com.helltractor.demo.redis;
+
+public interface RedisCache {
+    
+    interface Topic {
+        
+        String TOPIC_ONE = "__topic_one__";
+        
+        String TOPIC_TWO = "__topic_two__";
+    }
+    
+    interface Key {
+        
+        String KEY_ONE = "__key_one__";
+        
+        String KEY_TWO = "__key_two__";
+        
+        String KEY_THREE = "__key_three__";
+    }
+}
